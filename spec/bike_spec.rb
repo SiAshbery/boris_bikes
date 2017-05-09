691b0223f778284c3f8e1cb8a0caff105858a701
Mário Ribeiro <mail@marioribeiro.com>
2017-05-09 16:14:52 +0100

    Add test for release_bike and bike.working?

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,4 +2,8 @@ require 'bike'
 
 describe Bike do
   it { is_expected.to respond_to :working? }
+  it "Check if the bikes is working" do
+    bike = Bike.new
+    expect(bike.working?).to be
+  end
 end
