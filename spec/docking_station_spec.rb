dd367d830f78f1afc4faa7f3a1aef82624e360d0
Simon Ashbery <si.ashbery@gmail.com>
2017-05-09 15:54:15 +0100

    Created(not wokring)  test case for creating a new bike and updated the docking station spec and docking station to require bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it "Creates a new Bike Object" do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike).to exist
+  end
 end
