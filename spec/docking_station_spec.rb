c5b704b72564a9801d983b5ef17ad051ae411012
Simon Ashbery <si.ashbery@gmail.com>
2017-05-09 12:08:15 +0100

    Created expected case for release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,5 +2,6 @@ require 'docking_station'
 
 describe DockingStation do
   it 'releases a bike' do
-    expect(docking_station.release)
+    expect(docking_station).to respond_to(:release_bike)
+  end
 end
