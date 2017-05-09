36ca90be3c7c8bac14091c5541e452f22f4014d8
Simon Ashbery <si.ashbery@gmail.com>
2017-05-09 12:37:04 +0100

    Created passing test for release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,6 @@ require 'docking_station'
 
 describe DockingStation do
   it 'releases a bike' do
-    expect(docking_station).to respond_to(:release_bike)
+    expect(subject).to respond_to(:release_bike)
   end
 end
