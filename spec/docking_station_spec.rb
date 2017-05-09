2b436784d402bebe8cb16f8a9141347c55d049b4
Mário Ribeiro <mail@marioribeiro.com>
2017-05-09 16:36:10 +0100

    Add code to pass bike.working? test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,9 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-  it "Creates a new Bike Object" do
-    docking_station = DockingStation.new
-    expect(docking_station.release_bike).to be_a Bike
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
 end
